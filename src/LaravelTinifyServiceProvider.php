18c476cbc8e115c5432930f29ca98ca18c29a1c4
Bhavin Rudani <bhavin@brandzgarage.com>
2021-04-22 00:07:33 +0530

    added laravel 8 compatibility

--- src/LaravelTinifyServiceProvider.php
+++ src/LaravelTinifyServiceProvider.php
@@ -1,55 +1,57 @@
-<?php 
+<?php
 
 namespace msonowal\LaravelTinify;
 
 use Illuminate\Support\ServiceProvider;
 use Tinify\Tinify;
 
-class LaravelTinifyServiceProvider extends ServiceProvider {
+class LaravelTinifyServiceProvider extends ServiceProvider
+{
 
-	/**
-	* Indicates if loading of the provider is deferred.
-	*
-	* @var bool
-	*/
-	protected $defer = false;
+    /**
+    * Indicates if loading of the provider is deferred.
+    *
+    * @var bool
+    */
+    protected $defer = false;
 
-	/**
-	* Register custom form macros on package start
-	* @return void
-	*/
-	public function boot()
+    /**
+    * Register custom form macros on package start
+    * @return void
+    */
+    public function boot()
     {
         $this->publishConfiguration();
     }
 
-	/**
-	* Register the service provider.
-	*
-	* @return void
-	*/
-	public function register()
-	{
-
-		$config = __DIR__ . '/../config/tinify.php';
-        $this->mergeConfigFrom($config, 'tinify');
-		$this->app->bind('tinify', 'msonowal\LaravelTinify\Services\TinifyService');
-
-	}
-
-	/**
-	* Get the services provided by the provider.
-	*
-	* @return array
-	*/
-	public function provides()
-	{
-		return array();
-	}
-	public function publishConfiguration()
+    /**
+    * Register the service provider.
+    *
+    * @return void
+    */
+    public function register()
     {
-        $path   =   realpath(__DIR__.'/../config/tinify.php');
-        $this->publishes([$path => config_path('tinify.php')], 'config');
+        $config = __DIR__ . '/../config/tinify.php';
+        $this->mergeConfigFrom($config, 'tinify');
+        $this->app->bind('tinify', 'msonowal\LaravelTinify\Services\TinifyService');
     }
 
+    /**
+    * Get the services provided by the provider.
+    *
+    * @return array
+    */
+    public function provides()
+    {
+        return [];
+    }
+
+    public function publishConfiguration()
+    {
+        $this->publishes([
+            __DIR__.'/../config/tinify.php' => config_path('tinify.php'),
+        ], 'config');
+        // $path   =   realpath(__DIR__.'/../config/tinify.php');
+        // $this->publishes([$path => config_path('tinify.php')], 'config');
+    }
 }
