d117e59aff601a8ee5113f10a839af4d764f6da6
Bhavin Rudani <bhavinrudani94@gmail.com>
2022-03-30 21:05:19 +0530

    Laravel 8.* compatibility added (#10)
    * added laravel 8 compatibility
    
    * Update LaravelTinifyServiceProvider.php
    
    Co-authored-by: Bhavin Rudani <bhavin@brandzgarage.com>

--- src/Facades/Tinify.php
+++ src/Facades/Tinify.php
@@ -1,8 +1,13 @@
-<?php 
+<?php
+
 namespace msonowal\LaravelTinify\Facades;
+
 use Illuminate\Support\Facades\Facade;
-class Tinify extends Facade {
-	protected static function getFacadeAccessor(){
-		return 'tinify';
-	}
+
+class Tinify extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return 'tinify';
+    }
 }
